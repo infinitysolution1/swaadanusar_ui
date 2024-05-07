03f3e4a866f8eedf9ff9b3d07b36ce7b162859ef
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-05-08 03:00:33 +0530

    navbar and footer ui

---
+++ components/Footer.jsx
@@ -0,0 +1,182 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { LuBuilding } from "react-icons/lu";
+import { MdOutlineMail } from "react-icons/md";
+import { IoMdCall } from "react-icons/io";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { useRouter } from "next/router";
+
+let footerItemsClass =
+  "text-base lg:text-sm hover:underline text-quirkyReddishOrange/80";
+
+const navbarItems = [
+  {
+    title: "Why Liquid Salt ?",
+    link: "/",
+  },
+  {
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    title: "R&D",
+    link: "/research",
+  },
+  {
+    title: "Blogs",
+    link: "/blogs",
+  },
+  {
+    title: "Contact Us",
+    link: "/contact",
+  },
+];
+
+const webItems = [
+  {
+    title: "Terms & Conditions",
+    link: "/terms",
+  },
+  {
+    title: "Privacy Policy",
+    link: "/privacy",
+  },
+  {
+    title: "Disclaimer",
+    link: "/disclaimer",
+  },
+  {
+    title: "FAQ",
+    link: "/faq",
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer class="bg-[#fff] border-[2px] border-logoOrange">
+      <div class=" max-w-screen  bg-cream space-y-4 bg- px-4 pt-8 sm:px-6 lg:px-[5%]">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <div class=" w-[50vw] h-[10vh] md:w-[12.5vw] relative">
+            <Image src={"/swaadlogo.png"} layout="fill" />
+          </div>
+        </div>
+        <div className="my-8 h-[1px] w-full border-b-[1px] border-quirkyReddishOrange/20" />
+
+        <div className="container  mx-auto w-full flex flex-col lg:flex-row flex-wrap justify-between items-top">
+          <div className="w-full  lg:w-[22.5%] h-full flex flex-col gap-y-4">
+            <a className="flex flex-row items-center gap-x-2">
+              <div className="px-2 py-2 border-quirkyReddishOrange border-[1px] rounded-lg">
+                <MdOutlineMail className="text-quirkyReddishOrange text-sm" />
+              </div>
+              <p className={`${footerItemsClass}`}>{" evocare@gmail.com"}</p>
+            </a>
+            <a className="flex flex-row items-center gap-x-2">
+              <div className="px-2 py-2 border-quirkyReddishOrange border-[1px] rounded-lg">
+                <IoMdCall className="text-quirkyReddishOrange text-sm" />
+              </div>
+              <p className={`${footerItemsClass} `}>
+                {" +91-633431023, 0141-22987671"}
+              </p>
+            </a>
+          </div>
+          <div className="w-full lg:w-[55%] mt-8 pl-[5%] lg:mt-0 grid grid-cols-2 md:grid-cols-3 mb-[15vh]">
+            <div className="w-full flex flex-col items-left">
+              <h3 className="text-xl font-semibold text-quirkyReddishOrange">
+                Quick Links
+              </h3>
+              <ul className="mt-2">
+                {navbarItems.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <Link
+                        href={item.link}
+                        onClick={() => router.push(item.link)}
+                        className={footerItemsClass}
+                      >
+                        {item.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="w-full flex flex-col items-left ">
+              <h3 className="text-xl font-semibold text-quirkyReddishOrange">
+                Website
+              </h3>
+              <ul className="mt-2">
+                {webItems.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <Link
+                        href={item.link}
+                        onClick={() => router.push(item.link)}
+                        className={footerItemsClass}
+                      >
+                        {item.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="w-full flex flex-col items-left mt-[10%] md:mt-0">
+              <h3 className="text-xl font-semibold text-quirkyReddishOrange">
+                Social Links
+              </h3>
+              <div className="flex flex-row justify-start gap-x-2 mt-2">
+                <a
+                  onClick={() => {}}
+                  className="p-2 border-quirkyReddishOrange rounded-md border-[1px]"
+                >
+                  <FaFacebookF className="text-quirkyReddishOrange text-sm" />
+                </a>
+                <a
+                  onClick={() => {}}
+                  className="p-2 border-quirkyReddishOrange rounded-md border-[1px]"
+                >
+                  <FaTwitter className="text-quirkyReddishOrange text-sm" />
+                </a>
+                <a
+                  onClick={() => {}}
+                  className="p-2 border-quirkyReddishOrange rounded-md border-[1px]"
+                >
+                  <FaLinkedinIn className="text-quirkyReddishOrange text-sm" />
+                </a>
+                <a
+                  onClick={() => {}}
+                  className="p-2 border-quirkyReddishOrange rounded-md border-[1px]"
+                >
+                  <FaInstagram className="text-quirkyReddishOrange text-sm" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border-t-[1px] border-quirkyReddishOrange/30 mt-6 py-4 flex flex-col-reverse items-center  md:flex-row md:justify-between">
+          <div className="w-3/12" />
+          <p className="text-center text-sm text-quirkyReddishOrange">
+            &copy; {new Date().getFullYear()}{" "}
+            {"Swaad Anusar. All rights reserved."}
+          </p>
+          <div className="flex flex-row text-sm justify-center w-full md:w-3/12 mb-4 md:mb-0 text-quirkyReddishOrange">
+            <p className="text-[12px]">Developed and Maintained By </p>
+            <a
+              href="https://theinfinitysolutions.net"
+              className="underline ml-2 font-bold text-[12px] text-quirkyReddishOrange"
+            >
+              The Infinity Solutions
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { MdCall } from "react-icons/md";
+import { MdOutlineMenu } from "react-icons/md";
+
+const customTextColor = "#ff7f9b";
+
+let navbarItemsClass =
+  "relative z-10 text-black text-sm block after:block after:content-[''] after:absolute after:h-[2px] after:text-logoOrange after:bg-logoOrange after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center cursor-pointer";
+
+// const navbarItems = [
+//   { title: "Home", link: "/" },
+//   { title: "About Us", link: "/" },
+//   { title: "Services", link: "/" },
+//   { title: "Our Doctors", link: "/" },
+//   { title: "Blogs", link: "/" },
+//   { title: "Contact Us", link: "/" },
+// ];
+
+const navbarItems = [
+  {
+    title: "Why Liquid Salt ?",
+    link: "/",
+  },
+  {
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    title: "R&D",
+    link: "/research",
+  },
+  {
+    title: "Blogs",
+    link: "/blogs",
+  },
+  {
+    title: "Contact Us",
+    link: "/contact",
+  },
+];
+
+const Navbar = ({}) => {
+  const router = useRouter();
+  const [currentHover, setCurrentHover] = useState("");
+
+  return (
+    <nav className="flex flex-row w-[100vw] border-primaryColor h-[15vh] md:h-[20vh] fixed top-0 items-center justify-between py-6 bg-cream px-4 lg:px-[5%] z-50 shadow-md">
+      <div className=" w-5/12 md:w-3/12 h-full hidden md:flex flex-row justify-end items-center "></div>
+      <div className=" w-5/12 md:w-2/12 h-full flex md:hidden flex-row justify-end items-end "></div>
+      <div className=" md:flex w-full md:w-6/12 md:px-8 flex flex-col items-center">
+        <div className="flex flex-row items-center justify-center gap-x-4">
+          <div className=" w-[30vw] h-[7vh] md:h-[9vh] md:w-[12vw] relative">
+            <Image src={"/swaadlogo.png"} layout="fill" />
+          </div>
+        </div>
+        <div className=" hidden md:flex w-full  mt-6 flex-row justify-between items-baseline ">
+          {navbarItems.map((item, index) => {
+            return (
+              <Link
+                onMouseEnter={() => {
+                  setCurrentHover(item.title);
+                }}
+                key={index}
+                href={item.link}
+                onClick={() => {
+                  router.push(item.link + "");
+                }}
+                className={`${navbarItemsClass} `}
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className=" w-5/12 md:w-3/12 h-full hidden md:flex flex-row justify-end items-end ">
+        <button
+          onClick={() => {
+            router.push("/appointments");
+          }}
+          className="px-2 md:px-8 h-[40px] font-semibold border-[2px] border-logoGreen bg-logoGreen text-xs md:text-sm text-white rounded-full"
+        >
+          Try Liquid Salt
+        </button>
+        <button className=" px-1 md:px-2 ml-2 h-[40px] border-[2px] border-logoGreen bg-logoGreen/5 text-sm text-white rounded-full">
+          <MdCall className="text-logoGreen text-xl" />
+        </button>
+      </div>
+      <div className=" w-5/12 md:w-2/12 h-full flex md:hidden flex-row justify-end items-center ">
+        <button className=" px-1 md:px-2 ml-2 h-[40px]  bg-logoGreen/5 text-sm text-white rounded-full">
+          <MdOutlineMenu className="text-logoGreen text-xl" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
