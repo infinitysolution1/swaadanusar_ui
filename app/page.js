4e98a73e8d5196dcf7fe8589e65d9e9e99350a74
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-05-16 18:31:37 +0530

    adding page and font file

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ import Testimonials from "@/components/Testimonials";
 import AsSeenOn from "@/components/AsSeenOn";
 import ContactUs from "@/components/ContactUs";
 import Blogs from "@/components/Blogs";
+import localFont from "next/font/local";
+
+const sumac = localFont({ src: "../public/sumac-timber.woff2" });
 
 export default function Home() {
   return (
@@ -23,27 +26,27 @@ export default function Home() {
         </div>
         <div className="flex flex-col md:flex-row w-full h-full bg-quirkyMarineBlue/10 relative items-center p-8 justify-between pt-[15vh] px-[5vw]">
           <div className="flex flex-col items-center w-full md:w-1/2 h-full justify-center">
-            <div className="flex flex-col items-center">
-              <div className="flex h-[8vh] w-[8vh] relative">
-                <Image
-                  src={"/evocarelogo.png"}
-                  objectFit="contain"
-                  layout="fill"
-                  objectPosition="bottom"
-                />
-              </div>
-              <div className="flex h-[30vh] w-[40vh] -mt-6 relative">
+            <div className="flex flex-col items-start px-[5vw]">
+              <div className="flex h-[12.5vh] w-[20vh] relative">
                 <Image
                   src={"/swaadlogo.png"}
                   objectFit="contain"
                   layout="fill"
                 />
               </div>
-              <p className="text text-sm md:mx-[15vh] text-center font-light text-darkBrown">
+              <h2
+                className={` ${sumac.className} text-[4.5rem] font-semibold leading-[4rem] text-logoGreen pr-8`}
+              >
+                India's first Alkaline liquid salt
+              </h2>
+              <p className="text text-md text-center md:text-start text-darkBrown mt-4">
                 {
                   "Transforming Taste, Elevating Health: Introducing Swaad Anusar, India's Pioneer Liquid Alkaline Salt!"
                 }
               </p>
+              <button className="bg-logoGreen border-[2px] border-logoOrange text-white px-8 py-2 mt-4 rounded-full">
+                Try Liquid Salt
+              </button>
             </div>
           </div>
           <div className="flex flex-col items-center w-full md:w-1/2 h-full justify-center relative">
@@ -51,9 +54,7 @@ export default function Home() {
               <div className="flex h-[40vh] w-[25vh] md:h-[60vh] md:w-[35vh] z-10 relative">
                 <Image src={"/product.png"} objectFit="contain" layout="fill" />
               </div>
-              <div className=" absolute animate-rotate right-[27.5%] z-20 bottom-0 md:bottom-[12.5vh] w-[10vh] h-[10vh] md:w-[15vh] md:h-[15vh]  ">
-                <Image src={"/asset4.png"} objectFit="contain" layout="fill" />
-              </div>
+
               <div className="absolute right-[15%] md:right-[22.5%] z-0 top-[10vh] flex justify-center items-center h-[12.5vh] w-[12.5vh] md:h-[20vh] md:w-[20vh]">
                 <Image
                   src={"/asset2.svg"}
