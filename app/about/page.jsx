2766f553995430435bf7ce93797c4e070afd7743
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-12 09:47:49 +0530

    minor fixes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CTAVetical from "@/components/CTAVetical";
+import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa";
 
 const AboutUs = () => {
   return (
@@ -21,6 +23,67 @@ const AboutUs = () => {
         </div>
         <div className=" w-full md:w-[70%] mt-8 lg:mt-0 flex flex-col items-start">
           <div className=" flex flex-col items-start w-full border-[1px] border-darkBrown overflow-hidden rounded-2xl">
+            <div className=" flex flex-col items-start w-full border-b-[1px] bg-quirkyMarineBlue/20  border-darkBrown px-4 py-4">
+              <h3 className="text-xl font-bold text-logoGreen">DIRECTORS</h3>
+            </div>
+            <div className="w-full flex flex-col px-4 py-8">
+              <div className=" flex flex-col lg:flex-row justify-start w-full items-center">
+                <div className="h-[25vh] w-[25vh] relative rounded-md overflow-hidden">
+                  <Image src={"/director1.png"} layout="fill" />
+                </div>
+                <div className=" flex flex-col items-center lg:items-start mt-8 lg:mt-0 w-full lg:w-8/12 lg:ml-8">
+                  <div className="flex flex-row w-full justify-center lg:justify-start items-center ">
+                    <p className=" text-logoGreen text-center lg:text-start text-xl font-medium">
+                      {"Shubham Jain"}
+                    </p>
+                    <a
+                      href="https://in.linkedin.com/in/shubham-jain-067914169"
+                      target="_blank"
+                      className=" lg:ml-2"
+                    >
+                      <FaLinkedin className="text-xl text-logoGreen lg:ml-2" />
+                    </a>
+                  </div>
+                  <p className="text-darkBrown text-center lg:text-start text-md">
+                    {"Director"}
+                  </p>
+
+                  <p className="text-darkBrown text-center lg:text-start text-md">
+                    {
+                      "The mastermind behind Evocare Essentials has been fascinated by salt since he was young, and his vision was to revolutionize the Salt industry with speciality liquid salt. A healthier table salt for everyone. Following his family's salt industry legacy, he graduated from Mumbai University and is now pursuing an MBA in Operations Management. He brings 4 years of field experience alongside his own initiatives."
+                    }
+                  </p>
+                </div>
+              </div>
+              <div className=" flex flex-col lg:flex-row justify-start items-center mt-[5vh]">
+                <div className="h-[25vh] w-[25vh] relative rounded-md overflow-hidden">
+                  <Image src={"/director2.png"} layout="fill" />
+                </div>
+                <div className=" flex flex-col items-center lg:items-start mt-8 lg:mt-0 w-full lg:w-8/12 lg:ml-8">
+                  <div className="flex flex-row justify-start items-center ">
+                    <p className=" text-logoGreen text-xl font-medium">
+                      {"Ashika Jain"}
+                    </p>
+                    <a
+                      href="https://in.linkedin.com/in/shubham-jain-067914169"
+                      target="_blank"
+                      className=" ml-2"
+                    >
+                      <FaLinkedin className="text-xl text-logoGreen ml-2" />
+                    </a>
+                  </div>
+                  <p className="text-darkBrown text-md">{"Director"}</p>
+
+                  <p className="text-darkBrown text-center lg:text-start  text-md">
+                    {
+                      "Ashika Jain, is the marketing and creative strategist behind Evocare Essentials. She has 8 years of accounting experience . Leveraging her family's 35-year salt legacy, she is also a leader adept at team management and business organization."
+                    }
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" flex flex-col items-start w-full border-[1px] border-darkBrown overflow-hidden mt-[5vh] rounded-2xl">
             <div className=" flex flex-col items-start w-full border-b-[1px] bg-quirkyMarineBlue/20  border-darkBrown px-4 py-4">
               <h3 className="text-xl font-bold text-logoGreen">
                 PROBLEM IN THE INDUSTRY
