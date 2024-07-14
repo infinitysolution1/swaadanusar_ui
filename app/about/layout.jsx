410a0d813ba5309804e3ae92f9908f9a6994cd5e
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-14 12:51:47 +0530

    layout changes

---
+++ app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "About Us | Swaad Anusar",
+  description: "India's first Alkaline liquid salt",
+};
+
+const Layout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
